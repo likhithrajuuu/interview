4613f7265651c0252be48cb27563bf7dbb80a606
Likhith Raju P <likhithraju0@gmail.com>
2025-08-04 18:02:58 +0530

    Add reportWebVitals and logging utility to the frontend
    - Implemented reportWebVitals function to measure performance metrics.
    - Added a logging utility with different log levels (info, warning, error) to enhance debugging capabilities.

--- interview-frontend/src/api/customerApi.js
+++ interview-frontend/src/api/customerApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8080/api/customers";
 
+// Get paginated customers
 export const getAllCustomers = async (page = 0, size = 10) => {
   const response = await axios.get(`${BASE_URL}/get/all`, {
     params: { page, size }
@@ -9,6 +10,7 @@ export const getAllCustomers = async (page = 0, size = 10) => {
   return response.data;
 };
 
+// Get customer by ID
 export const getCustomerById = async (id) => {
   const response = await axios.get(`${BASE_URL}/${id}`);
   return response.data;
